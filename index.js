76582a5fa6929dd037e71c037749835eda4ff142
Mateus Pinheiro <mateus.pinheiro@gmail.com>
2018-06-18 21:21:21 -0300

    Finishes the sprite maker! It's alive!

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const tmp         = require('tmp')
-const fs          = require('fs')
-const svgstore    = require('svgstore')
-const find        = require('./lib/find.js')
-const download    = require('./lib/download.js')
+const tmp       = require('tmp')
+const find      = require('./lib/find.js')
+const download  = require('./lib/download.js')
+const svgSprite = require('./lib/svgSprite.js')
 
 module.exports = {
 	downloadAll: (config, path) => {
@@ -14,31 +13,13 @@ module.exports = {
 		})
 	},
 
-	downloadSprite: (config, spriterConfig, path) => {
+	downloadSprite: (config, dest) => {
 		return new Promise((resolve, reject) => {
 			const tmpDir = tmp.dirSync()
-			const sprite = svgstore()
-
-			console.log(tmpDir.name)
 			
 			find(config.token, config.fileId)
 			.then(icons => download(icons, tmpDir.name))
-			.then(icons => {
-				const files = fs.readdirSync(tmpDir.name)
-				// Dont know why, but it is not reading from
-				// the dir when there is clearly stuff there.
-				files.forEach(file => {
-					const filePath = `${tmpDir.name}/${file}`
-					const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' })
-
-					console.log(fileContent)
-					sprite.add(file, fileContent)
-				})
-				
-				console.log(sprite.toString())
-				fs.writeFileSync(`${dest}/sprite.svg`, sprite.toString())
-				return true
-			})
+			.then(() => svgSprite(tmpDir.name, dest))
 			.then(() => resolve())
 			.catch(error => reject(error))
 		})
